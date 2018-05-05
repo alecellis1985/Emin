38753d07342ab56f51c6b932819aee595cb22f96
Alexander Ellis <alecellis1985@hotmail.com>
2018-05-04 21:22:57 -0300

    Second commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+(function () {
+	$('.dropdown').hover(
+		function () {
+			$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
+		},
+		function () {
+			$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
+		}
+	);
+
+	$('.dropdown-menu').hover(
+		function () {
+			$(this).stop(true, true);
+		},
+		function () {
+			$(this).stop(true, true).delay(200).fadeOut();
+		}
+	);
+
+	$('.imgStats').hover(
+		function () {
+			$(this).find('.text').stop(true, true).delay(200).fadeIn();
+		},
+		function () {
+			$(this).find('.text').stop(true, true).delay(200).fadeOut();
+		}
+	);
+
+})();
