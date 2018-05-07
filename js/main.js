a84d18efeb1fff09f86f0e8f25164c558992c97d
Alexander Ellis <alecellis1985@hotmail.com>
2018-05-07 20:46:41 -0300

    Responsive

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 (function () {
+	new WOW().init();
+
 	$('.dropdown').hover(
 		function () {
 			$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
@@ -25,5 +27,18 @@
 			$(this).find('.text').stop(true, true).delay(200).fadeOut();
 		}
 	);
+	$('.dropdown.display a').trigger('mouseover');
+
+	$('.dropdown.display a').on('click', function (event) {
+		$('.dropdown.display').css('height', '50px');
+		event.stopPropagation();
+		$('.dropdown.display a').trigger('mouseover');
+	})
+
+	$('.dropdown a').on('click', function (event) {
+		$('.dropdown').css('height', '50px');
+		event.stopPropagation();
+		$('.dropdown.display a').trigger('mouseover');
+	})
 
 })();
